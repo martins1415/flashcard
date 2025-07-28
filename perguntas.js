61432e45be51374cc79925a1f029d610c7382a83
martins1415 <luciano.jose.martins@escola.pr.gov.br>
2025-07-28 11:49:15 -0300

    lucijsqjhd

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+
+criaCartao(
+    historia,
+    Em que ano o Brasil foi “descoberto” pelos portugueses e por quem ?,
+    Em 1500, por Pedro Álvares Cabral
+)
+
+criaCartao(
+    historia
+    Qual foi a primeira capital do Brasil,
+    Salvador, na Bahia
+)
+
+criaCartao(
+    futebol
+    melhor time do brasil?,
+    corinthias
+)
